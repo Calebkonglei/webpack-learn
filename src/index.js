a1a91feaa88bc5a50cfb560e0d6be6bfe3953900
dannisi <13403891610@163.com>
2019-11-05 16:48:22 +0800

    开发环境

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import _ from 'lodash';
-import './style.css';
-import img from './switzer.jpg'
-import Data from './data.xml';
+// import './style.css';
+// import img from './switzer.jpg'
+// import Data from './data.xml';
+import printMe from './print';
 
 function component() {
 	let element = document.createElement('div');
+	var btn = document.createElement('button');
 
 	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-	element.classList.add('hello');
-	var myIcon = new Image();
-	myIcon.src = img;
+	btn.innerHTML = '点击这里，然后可以查看 console！';
+	btn.onclick = printMe;
+	// element.classList.add('hello');
+	// var myIcon = new Image();
+	// myIcon.src = img;
 
-	element.appendChild(myIcon);
+	// element.appendChild(myIcon);
 
-	console.log(Data)
+	// console.log(Data)
+	element.appendChild(btn);
 
 	return element;
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,89 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fs = require('fs-extra');
+const appDirectory = fs.realpathSync(process.cwd());
+// const CleanWebpackPlugin = require('clean-webpack-plugin');
+const pkg = require(resolveApp('package.json'));
+
+function resolveApp(relativePath) {
+    return path.resolve(appDirectory, relativePath);
+}
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist')
+	mode: 'development',
+	devtool: 'cheap-module-source-map',
+	devServer: {
+		headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, HEAD, DELETE, FETCH'
+        },
+		contentBase: './dist', // 告诉服务器从那个目录中提供内容
+		compress: true, // 	启动gzip压缩
+		// allowedHosts: [], // 白名单服务
+		clientLogLevel: 'none',
+		disableHostCheck: false, // 为true 容易受到dns攻击
+		historyApiFallback: pkg.noRewrite //
+            ? false
+            : {
+                  disableDotRule: true // 禁止url带小数点
+			  },
+		host: process.env.HOST || '0.0.0.0',
+		hot: true,
+		quiet: true, // 打开后，除了初识启动信息，其他内容不会打印到控制台
+		https: process.env.HTTPS === 'true',
+		overlay: false, // 是否在浏览器中显示编译错误
+		watchContentBase: true, // 文件修改，会触发一次完整的页面重载
+		// proxy: {
+		// 	'/api': {
+		// 	  target: 'http://localhost:3000',
+		// 	  pathRewrite: {'^/api' : ''}
+		// 	}
+		//   }
+		// lazy: true,
+		// filename: '[name].bundle.js' // 减少编译，使用filename 只会在某个文件被请求时编译
 	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					'css-loader'
-				]
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-					'file-loader'
-				]
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				use: [
-					'csv-loader'
-				]
-			},
-			{
-				test: /\.xml$/,
-				use: [
-					'xml-loader'
-				]
-			}
-		]
-	}
+	entry: {
+		app: './src/index.js',
+		print: './src/print.js',
+	},
+	plugins: [
+		// new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			title: '管理输出'
+		})
+	],
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/'
+	},
+	// module: {
+	// 	rules: [
+	// 		{
+	// 			test: /\.css$/,
+	// 			use: [
+	// 				'style-loader',
+	// 				'css-loader'
+	// 			]
+	// 		},
+	// 		{
+	// 			test: /\.(png|svg|jpg|gif)$/,
+	// 			use: [
+	// 				'file-loader'
+	// 			]
+	// 		},
+	// 		{
+	// 			test: /\.(woff|woff2|eot|ttf|otf)$/,
+	// 			use: [
+	// 				'csv-loader'
+	// 			]
+	// 		},
+	// 		{
+	// 			test: /\.xml$/,
+	// 			use: [
+	// 				'xml-loader'
+	// 			]
+	// 		}
+	// 	]
+	// }
 };
